81582b38ab2bd61ee451e833ecd1de0a3d33de75
Mihir Verma <mihir.verma@itential.com>
2023-01-04 23:30:54 +0530

    added code

--- hoisting.js
+++ hoisting.js
@@ -4,5 +4,13 @@
 3. let and const are block scoped and var is functional scoped
 4. let and const are hoisted but in seperate memory space (known as script area) 
 */
+
+function varhoisting(params) {
+    console.log(a)
+    setTimeout(() => {
+        console.log("inside settimeout")
+    }, 1000);
+    var test = 10;
+}
 console.log(num);
 let num  = 10;
